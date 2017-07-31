89ec42fd5ff02892c5f6f7bd7df5184eead4e2b6
Raul Riera <raul.riera@shopify.com>
2017-07-31 13:45:28 -0400

    Created the project

---
+++ FunctionalTableData/FunctionalTableData.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalTableData.h
+//  FunctionalTableData
+//
+//  Created by Raul Riera on 2017-07-31.
+//  Copyright © 2017 Shopify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalTableData.
+FOUNDATION_EXPORT double FunctionalTableDataVersionNumber;
+
+//! Project version string for FunctionalTableData.
+FOUNDATION_EXPORT const unsigned char FunctionalTableDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalTableData/PublicHeader.h>
+
+
