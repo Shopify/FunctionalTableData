4cba852ff11bc3efb84387b30a559e2761e0b3e2
Raul Riera <raul.riera@shopify.com>
2017-08-02 11:09:28 -0400

    Typo

--- Examples/BasicCellsViewController.swift
+++ Examples/BasicCellsViewController.swift
@@ -27,7 +27,7 @@ class BasicCellsViewController: UITableViewController {
 		let cells: [CellConfigType] = [
 			// Since this cell is more complex than `LabelCell`, pass the updateView method into cellUpdater instead of making any updates inline.
 			SubtitleCell(key: "subtitleCell",
-			             state: SubtitleState(title: "Functional Table Data", subtitle: "Instead of trying to build many different UITableViewCells and implement a UITableView(DataSource|Delegate) for each view that then needs to understand all possible state transformations. Provide a system that let’s you express this as a series of states describing the cells themselves."),
+			             state: SubtitleState(title: "Functional Table Data", subtitle: "Instead of trying to build many different UITableViewCells and implement a UITableView(DataSource|Delegate) for each view that then needs to understand all possible state transformations. Provide a system that lets you express this as a series of states describing the cells themselves."),
 			             cellUpdater: SubtitleState.updateView)
 		]
 		
