46310183b49dd90523c8b4dfb32b6ac02923318c
Robin Senior <robin.senior@shopify.com>
2018-01-05 10:20:21 -0500

    Change optional State parameter of cellUpdate to a CellUseCase enum to be more explicit as to when cells should update to a new state or to prepare for reuse

--- FunctionalTableData/HostCell.swift
+++ FunctionalTableData/HostCell.swift
@@ -11,6 +11,11 @@ import UIKit
 /// Defines the view, state and layout information of a row item inside a TableSection.
 /// It relies on you to build UIView subclasses and use those instead of implementing UITableViewCell or UICollectionViewCell subclasses. This has the side effect of building better more reusable view components. This greatly simplifies composition by combining several host-cells into more complex layouts. It also makes equality simpler and more "Swifty" by requiring that anything provided as State only requires that the State object conform to the Equatable protocol. The View portion of the generic only requires it to be a UIView subclass.
 public struct HostCell<View, State, Layout>: CellConfigType where View: UIView, State: Equatable, Layout: TableItemLayout {
+	public enum CellUseCase {
+		case prepareForReuse
+		case update(State)
+	}
+	
 	public typealias TableViewCellType = TableCell<View, Layout>
 	public typealias CollectionViewCellType = CollectionCell<View, Layout>
 	
@@ -19,10 +24,10 @@ public struct HostCell<View, State, Layout>: CellConfigType where View: UIView, 
 	public let actions: CellActions
 	/// Contains the state information of a cell.
 	public let state: State
-	/// A function that updates a cell's view to match the current state. It receives two values, the view instance and an optional state instance. The purpose of this function is to update the view to reflect that of the given state. The reason that the state is optional is because cells may move into the reuse queue. When this happens they no longer have a state and the updater function is called giving the opportunity to reset the view to its default value.
-	public let cellUpdater: (_ view: View, _ state: State?) -> Void
+	/// A function that updates a cell's view to match the current state. It receives two values, the view instance and a CellUseCase instance. The purpose of this function is to update the view to reflect that of the given state, or prepare for reuse.
+	public let cellUpdater: (_ view: View, _ useCase: CellUseCase) -> Void
 	
-	public init(key: String, style: CellStyle? = nil, actions: CellActions = CellActions(), state: State, cellUpdater: @escaping (_ view: View, _ state: State?) -> Void) {
+	public init(key: String, style: CellStyle? = nil, actions: CellActions = CellActions(), state: State, cellUpdater: @escaping (_ view: View, _ useCase: CellUseCase) -> Void) {
 		self.key = key
 		self.style = style
 		self.actions = actions
@@ -55,7 +60,7 @@ public struct HostCell<View, State, Layout>: CellConfigType where View: UIView, 
 	public func dequeueCell(from tableView: UITableView, at indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(TableCell<View, Layout>.self, indexPath: indexPath)
 		cell.prepare = { [cellUpdater] view in
-			cellUpdater(view, nil)
+			cellUpdater(view, .prepareForReuse)
 		}
 		return cell
 	}
@@ -69,7 +74,7 @@ public struct HostCell<View, State, Layout>: CellConfigType where View: UIView, 
 	public func dequeueCell(from collectionView: UICollectionView, at indexPath: IndexPath) -> UICollectionViewCell {
 		let cell = collectionView.dequeueReusableCell(CollectionCell<View, Layout>.self, indexPath: indexPath)
 		cell.prepare = { [cellUpdater] view in
-			cellUpdater(view, nil)
+			cellUpdater(view, .prepareForReuse)
 		}
 		return cell
 	}
@@ -79,7 +84,7 @@ public struct HostCell<View, State, Layout>: CellConfigType where View: UIView, 
 	public func update(cell: UITableViewCell, in tableView: UITableView) {
 		guard let cell = cell as? TableViewCellType else { return }
 		
-		cellUpdater(cell.view, state)
+		cellUpdater(cell.view, .update(state))
 		// Only layout cells that aren't in the reuse pool
 		if cell.superview != nil && !cell.isHidden {
 			UIView.performWithoutAnimation {
@@ -90,7 +95,7 @@ public struct HostCell<View, State, Layout>: CellConfigType where View: UIView, 
 	
 	public func update(cell: UICollectionViewCell, in collectionView: UICollectionView) {
 		guard let cell = cell as? CollectionViewCellType else { return }
-		cellUpdater(cell.view, state)
+		cellUpdater(cell.view, .update(state))
 	}
 	
 	public func isEqual(_ other: CellConfigType) -> Bool {
@@ -107,7 +112,7 @@ public struct HostCell<View, State, Layout>: CellConfigType where View: UIView, 
 }
 
 extension HostCell {
-	public init<T: RawRepresentable>(key: T, style: CellStyle? = nil, actions: CellActions = CellActions(), state: State, cellUpdater: @escaping (_ view: View, _ state: State?) -> Void) where T.RawValue == String {
+	public init<T: RawRepresentable>(key: T, style: CellStyle? = nil, actions: CellActions = CellActions(), state: State, cellUpdater: @escaping (_ view: View, _ useCase: CellUseCase) -> Void) where T.RawValue == String {
 		self.init(key: key.rawValue, style: style, actions: actions, state: state, cellUpdater: cellUpdater)
 	}
 }
