dcf7b09243330085beba5e9d25a0a15c2ed79845
Elisa Kazan <elisa.kazan@shopify.com>
2018-04-17 16:56:23 -0400

    previewingcontext

--- FunctionalTableData/CollectionView/FunctionalCollectionData.swift
+++ FunctionalTableData/CollectionView/FunctionalCollectionData.swift
@@ -386,6 +386,13 @@ public class FunctionalCollectionData: NSObject {
 	internal func calculateTableChanges(oldSections: [TableSection], newSections: [TableSection], visibleIndexPaths: [IndexPath]) -> TableSectionChangeSet {
 		return TableSectionChangeSet(old: oldSections, new: newSections, visibleIndexPaths: visibleIndexPaths)
 	}
+
+    public func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        guard let indexPath = collectionView?.indexPathForItem(at: location), let cell = collectionView?.cellForItem(at: indexPath) else { return nil }
+        guard let cellConfig = self[indexPath],
+            let viewController = cellConfig.actions.previewingViewControllerAction?(cell, cell.convert(location, from: collectionView), previewingContext) else { return nil }
+        return viewController
+    }
 }
 
 extension UICollectionView {
