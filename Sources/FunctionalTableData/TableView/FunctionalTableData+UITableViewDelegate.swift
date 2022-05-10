2ea0352980a97e99d4430728bce99585634476f2
Michael Garfinkle <michael.garfinkle@shopify.com>
2022-05-10 10:19:20 -0700

    fixed bug where selection state status not always updating on multi-select

--- Sources/FunctionalTableData/TableView/FunctionalTableData+UITableViewDelegate.swift
+++ Sources/FunctionalTableData/TableView/FunctionalTableData+UITableViewDelegate.swift
@@ -85,6 +85,10 @@ extension FunctionalTableData {
 		}
 		
 		public func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+            
+            //This fixes bug when selecting cells quickly in list view; without this could sometimes get behaviour where tableview cell was selected on the screen, but cell state selection status was not update. This may be due to the fact that state status is updated when cellConfig?.actions.selectionAction function is called. This function is only called in didSelectRowAt function, and could potentially not always fire.
+            guard tableView.cellForRow(at: indexPath) != nil else { return nil }
+            
 			if tableView.indexPathForSelectedRow == indexPath {
 				return nil
 			}
